98c8cda8f4fe6e093139e18fc07c34cd287df884
Mavericksb <fanim@tiscali.it>
2017-03-05 20:01:35 +0100

    Added AudioManager support for Audio Focus managing. App is done and working.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,11 +1,15 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +20,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Handler;
 
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 import static android.support.v4.content.ContextCompat.getColor;
 
 /**
@@ -27,11 +35,34 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     private int backgroundColor;
     private MediaPlayer mMediaPlayer;
+    private AudioManager audioManager;
+    private AudioManager.OnAudioFocusChangeListener  audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
+                mMediaPlayer.pause();
+            } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mMediaPlayer.pause();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mMediaPlayer.start();
+            }
+
+        }
+
+
+    };
+
 
     public WordAdapter(Activity context, ArrayList<Word> words, int background) {
         super(context, 0, words);
         backgroundColor = background;
-        }
+
+        audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+
+
+    }
 
     @NonNull
     @Override
@@ -62,26 +93,39 @@ public class WordAdapter extends ArrayAdapter<Word> {
         int color = getColor(getContext(), backgroundColor);
         textContainer.setBackgroundColor(color);
 
+
+
+
         final ImageView playIcon = (ImageView) listItemView.findViewById(R.id.play_icon);
         playIcon.setBackgroundColor(color);
         playIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 releaseMediaPlayer();
-                mMediaPlayer = MediaPlayer.create(getContext(), currentWord.getAudioId());
-                mMediaPlayer.start();
+                int response = audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (response == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(getContext(), currentWord.getAudioId());
+                    mMediaPlayer.start();
+                }
                 mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mediaPlayer) {
                         releaseMediaPlayer();
                     }
                 });
+
+
+
             }
         });
 
 
         return listItemView;
     }
+
+
+
+
 
     public void releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
@@ -94,6 +138,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mMediaPlayer = null;
+
+            audioManager.abandonAudioFocus(audioFocusChangeListener);
         }
     }
 
